df26771c16c71e647558e2e9a617abd68eeb8158
amjm1402 <amjm1402@gmail.com>
2015-05-09 01:24:25 +0200

    Drawing util stubs
    DrawingFragment and DrawingView

---
+++ Galeria2/src/com/example/Galeria2/DrawingFragment.java
@@ -0,0 +1,14 @@
+package com.example.Galeria2;
+
+import android.app.Fragment;
+
+/**
+ * Fragment containing a {@link DrawingView}.
+ * @author Ala
+ * @since 09-05-2015
+ * */
+public class DrawingFragment extends Fragment {
+
+
+
+}
---
+++ Galeria2/src/com/example/Galeria2/DrawingView.java
@@ -0,0 +1,21 @@
+package com.example.Galeria2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Custom view that can be drawn on. Uses touch detection.
+ * @author Ala
+ * @since 09-05-2015
+ */
+public class DrawingView extends View {
+
+    /**
+     * Basic constructor
+     * @see View
+     */
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
