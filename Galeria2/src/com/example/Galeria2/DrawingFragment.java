abad0ef4216df7cbdb9c0da5473f355c08db15f0
amjm1402 <amjm1402@gmail.com>
2015-05-24 21:31:00 +0200

    Drawing util update
    User can now revert last move with back button.
    Added Shape class, which consists of a drawed path and paint.

--- Galeria2/src/com/example/Galeria2/DrawingFragment.java
+++ Galeria2/src/com/example/Galeria2/DrawingFragment.java
@@ -1,12 +1,12 @@
 package com.example.Galeria2;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.app.Fragment;
 
 /**
  * Fragment containing {@link DrawingView}.
@@ -16,8 +16,19 @@ import android.app.Fragment;
 
  public class DrawingFragment extends Fragment {
 
+    private DrawingView drawingView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.drawingfragment, container, false);
+
+        View myView = inflater.inflate(R.layout.drawingfragment, container, false);
+
+        drawingView = (DrawingView) myView.findViewById(R.id.view);
+
+        return myView;
+    }
+
+    public void passBackButton(){
+        drawingView.backButtonPressed();
     }
 }
