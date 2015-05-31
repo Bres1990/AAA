2904b5644a3cfb3d6c1efd82c5d98c113efc9295
Adam Poterałowicz <181710@student.pwr.edu.pl>
2015-06-01 00:14:41 +0200

    Revert "Color picker - ready :)"
    This reverts commit 084a09526e1dbfd087db8a054677dda8f883b805.

--- Galeria2/src/com/example/Galeria2/ColorPickerView.java
+++ Galeria2/src/com/example/Galeria2/ColorPickerView.java
@@ -3,8 +3,6 @@ package com.example.Galeria2;
 import android.content.Context;
 import android.graphics.*;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -18,10 +16,9 @@ public class ColorPickerView extends View {
 
     /* MEASURES */
 
-    private static int CENTER_X = 150;
-    private static int CENTER_Y = 150;
-    private static int CENTER_RADIUS = 45;
-    private static final float PI = 3.1415926f;
+    private static int CENTER_X =100;
+    private static int CENTER_Y=100;
+    private static int CENTER_RADIUS = 25;
 
 
 
@@ -43,58 +40,28 @@ public class ColorPickerView extends View {
         /* Paint now takes colours straight from shader */
         gradientPaint.setShader(tmp);
 
-        gradientPaint.setStrokeWidth(35);
+        gradientPaint.setStrokeWidth(25);
         gradientPaint.setStyle(Paint.Style.STROKE);
 
         centerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         centerPaint.setColor(Color.WHITE);
-        centerPaint.setStrokeWidth(15);
+        centerPaint.setStrokeWidth(5);
 
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-        /* resize view */
-        setMeasuredDimension(CENTER_X * 2, CENTER_Y * 2);
+        setMeasuredDimension(CENTER_X*2, CENTER_Y*2);
+
+
     }
-
-
-    private int ave(int src, int dst, float percentage) {
-        return src + java.lang.Math.round(percentage * (dst - src));
-    }
-
-    private int interpolate(float unit){
-        if (unit <= 0) {
-            return shaderColors[0];
-        }
-        if (unit >= 1) {
-            return shaderColors[shaderColors.length - 1];
-        }
-
-        /* split length*unit into two parts - fraction and integer */
-        float fractionalPart = unit * (shaderColors.length - 1);
-        int integerPart = (int) fractionalPart;
-        fractionalPart -= integerPart;
-
-        /* mix two colors that are next to each other in appropriate percentage */
-        /* fractional part - percentage */
-
-        int c0 = shaderColors[integerPart];
-        int c1 = shaderColors[integerPart+1];
-        int a = ave(Color.alpha(c0), Color.alpha(c1), fractionalPart);
-        int r = ave(Color.red(c0), Color.red(c1), fractionalPart);
-        int g = ave(Color.green(c0), Color.green(c1), fractionalPart);
-        int b = ave(Color.blue(c0), Color.blue(c1), fractionalPart);
-
-        return Color.argb(a, r, g, b);
-    }
-
 
     @Override
     protected void onDraw(Canvas canvas) {
 
         float radius = CENTER_X - gradientPaint.getStrokeWidth()*0.5f;
+
 
         canvas.translate(CENTER_X, CENTER_X);
         canvas.drawOval(new RectF(-radius, -radius, radius, radius), gradientPaint);
@@ -102,32 +69,5 @@ public class ColorPickerView extends View {
 
 
         super.onDraw(canvas);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        float currentX = event.getX() - CENTER_X;
-        float currentY = event.getY() - CENTER_Y;
-
-        switch (event.getAction()){
-
-            case MotionEvent.ACTION_MOVE:
-                /* oblicz dlugosc luku - jednostke */
-                float angle = (float)java.lang.Math.atan2(currentY, currentX);
-                // need to turn angle [-PI ... PI] into unit [0....1]
-                float unit = angle/(2*PI);
-                if (unit < 0) {
-                    unit += 1;
-                }
-
-                Log.v("ColorPicker", "unit: "+unit);
-                centerPaint.setColor(interpolate(unit));
-                invalidate();
-                break;
-
-        }
-
-        return true;
     }
 }
